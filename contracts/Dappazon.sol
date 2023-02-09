0dd4d99e3f4dca6dc4ae09f12170ae16ab4bf28b
ijlalansari1122 <ansariijlal90@gmail.com>
2023-02-09 18:15:39 +0500

    blockchain dev

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+contract Dappazon {
 
-contract Dappazon {}
+// code goes here
+
+
+address public owner;
+struct Item{
+
+
+
+uint256 id;
+string name;
+string category;
+string image;
+uint256 cost;
+uint256 rating;
+uint256 stock;
+
+}
+
+mapping(uint256 => Item) public items;
+
+
+constructor () public {
+   
+owner = msg.sender;
+
+}
+
+// List products
+function list(
+uint256 _id , 
+string memory _name ,
+string memory _category ,
+string memory _image ,
+uint256 _cost , 
+uint256 _rating , 
+uint256 _stock  
+
+)public{
+// code goes here
+
+
+
+// creat item struct
+Item memory item = Item(
+_id , 
+_name ,
+_category ,
+_image ,
+_cost ,
+_rating ,
+_stock);
+
+// save item struct
+items[_id] = item;
+
+
+
+}
+
+
+// Buy products
+
+
+
+
+
+// Withdraw products
+
+
+
+
+
+
+}
+
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,74 @@
+const { wait } = require("@testing-library/user-event/dist/utils")
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+let dappazon
+let deployer , buyer
+
+
+beforeEach(async ()=>{ 
+// setup accounts
+
+[deployer , buyer] = await ethers.getSigners()
+
+
+
+  // Deploy contract
+
+  const Dappazon = await ethers.getContractFactory("Dappazon")
+dappazon = await Dappazon.deploy()
+
+
+
+})
+
+describe("Deployment",()=>{
+
+  it('sets the owner', async () => {
+  
+    expect(await dappazon.owner()).to.equal(deployer.address)
+  })
+})
+  describe("Listing", () => {
+
+
+      beforeEach(async () => {
+
+      transaction=  await dappazon.connect(deployer).list(
+1,
+"Shoes",
+"Clothing",
+"Image",
+1,
+4,
+5
+  )
+await transaction.wait()
+      })
+        
+        it('Returns item attribute', async () => {
+
+const item = await dappazon.items(1)
+
+expect(item.id).to.equal(1)
+
+
+
+
+
+    })
+
+
+
+
+
+
+  })
+
 
 })
